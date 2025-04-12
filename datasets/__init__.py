f77384e2a68ec08de91b3a0f96995831535b8541
xp@90 <xxxupeng@zju.edu.cn>
2025-04-12 17:50:23 +0800

    add DrivingStereo

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -2,3 +2,4 @@ from .sceneflow import SceneFlow_Dataset
 from .kitti import KITTI2015_Dataset, KITTI2012_Dataset
 from .middleburyeval3 import MiddleburyEval3_Dataset
 from .eth3d import ETH3D_Dataset
+from .drivingstereo import DrivingStereo_Dataset
