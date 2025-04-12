d619171c8adf2a988c35698b3f56dfe85eb554c4
xp@90 <xxxupeng@zju.edu.cn>
2025-04-12 18:37:25 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "stereo_toolbox",
+    version = "0.0.1",
+    keywords = ["pip", "stereo matching"],
+    description = "stereo toolbox.",
+    long_description = "A comprehensive stereo matching toolbox for efficient development and research.",
+    license = "MIT Licence",
+
+    url = "https://github.com/xxxupeng/stereo_toolbox",
+    author = "xxxupeng",
+    author_email = "xxxupeng@zju.edu.cn",
+
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = "any",
+    install_requires = ["numpy", "pillow", "torch", "opencv-python", "matplotlib", "jupyter", "matplotlib", "cmapy", "torchvision"]
+)
