e9fb17f92aad21f87b067712b3e6c6dea6350a16
xp@90 <xxxupeng@zju.edu.cn>
2025-05-01 22:36:58 +0800

    version 0.2.0

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
 #-*- coding:utf-8 -*-
 
+# python setup.py sdist
+# twine upload ./dist/*
+
 from setuptools import setup, find_packages
 
 setup(
     name = "stereo_toolbox",
-    version = "0.1.5",
+    version = "0.2.0",
     keywords = ["pip", "stereo matching"],
     description = "stereo toolbox.",
     long_description = "A comprehensive stereo matching toolbox for efficient development and research.",
@@ -18,5 +21,5 @@ setup(
     packages = find_packages(),
     include_package_data = True,
     platforms = "any",
-    install_requires = ["numpy", "pillow", "torch", "opencv-python", "matplotlib", "jupyter", "matplotlib", "cmapy", "torchvision", 'xformers']
+    install_requires = ["numpy", "pillow", "torch", "opencv-python", "matplotlib", "jupyter", "matplotlib", "cmapy", "torchvision", 'xformers', 'accelerate']
 )
