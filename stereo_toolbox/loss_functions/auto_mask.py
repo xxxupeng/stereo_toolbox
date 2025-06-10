c208de735e718c8985379237a0fdecf123cd8bc3
xp@90 <xxxupeng@zju.edu.cn>
2025-06-10 16:38:01 +0800

    mask occlusion by photometric threahold

--- stereo_toolbox/loss_functions/auto_mask.py
+++ stereo_toolbox/loss_functions/auto_mask.py
@@ -4,7 +4,7 @@ import torch.nn.functional as F
 from .photometric_loss import photometric_loss
 
 
-def auto_mask(left_image, right_image, disp, denorm=False):
+def auto_mask(left_image, right_image, disp, denorm=False, stationary=True, occlusion=False):
     if denorm:
         mean = torch.tensor([0.485, 0.456, 0.406], device=left_image.device).view(1, 3, 1, 1)
         std = torch.tensor([0.229, 0.224, 0.225], device=left_image.device).view(1, 3, 1, 1)
@@ -12,6 +12,15 @@ def auto_mask(left_image, right_image, disp, denorm=False):
         right_image = right_image * std + mean
 
     reproj_error = photometric_loss(left_image, right_image, disp.detach(), enable_mask=False)
-    identity_error = photometric_loss(left_image, right_image, enable_mask=False)
 
-    return reproj_error < identity_error
+    mask = torch.ones_like(reproj_error, dtype=torch.bool)
+
+    if stationary:
+        identity_error = photometric_loss(left_image, right_image, enable_mask=False)
+        mask = mask * (reproj_error < identity_error)
+
+    if occlusion:
+        occlusion_threshold = reproj_error.mean(dim=(-1,-2), keepdim=True) * 2
+        mask = mask * (reproj_error < occlusion_threshold)
+
+    return mask
