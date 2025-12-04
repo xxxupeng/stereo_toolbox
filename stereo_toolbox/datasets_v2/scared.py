2ae69eaef8052c4d684540d0bc08a0edd47524bd
xp@90 <xxxupeng@zju.edu.cn>
2025-12-04 15:42:05 +0800

    fix scared dataloader

--- stereo_toolbox/datasets_v2/scared.py
+++ stereo_toolbox/datasets_v2/scared.py
@@ -11,6 +11,14 @@ from .stereodataset import Stereo_Dataset
 """
 https://github.com/dimitrisPs/scared_toolkit
 please refer to this repo for more details about SCARED dataset.
+
+python -m scripts.generate_keyframe_dataset root_dir [--out_dir] --recursive --depth --undistort --disparity --alpha 0
+
+disparity is saved in 16-bit png format, need to divide by 256 to get the actual disparity values.
+
+Disparity has a scale issue, multiplication by 2 is needed for alignment (not analyzed thoroughly yet).
+The largest 1% disparity values are removed to eliminate noise.
+
 """
 class SCARED_Dataset(Stereo_Dataset):
     def load_image_list(self, data_path='/data1/xp/SCARED/'):
@@ -36,6 +44,10 @@ class SCARED_Dataset(Stereo_Dataset):
         
         disp = Image.open(filename)
         disp = np.array(disp, dtype=np.float32) / 256.
+
+        disp = disp * 2.0  # scale issue in SCARED dataset
+        disp[disp > np.percentile(disp, 99)] = 0  # remove the largest 1% disparity values to eliminate noise
+
         return disp
 
 
